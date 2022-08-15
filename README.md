b30720f309973951234963ef88208c2cf972a029
wegged <wegged@gmail.com>
2022-08-15 10:23:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-l64zv7-tw6ybq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-l64zv7-tw6ybq)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css'],
+})
+export class ProductAlertsComponent implements OnInit {
+  constructor() {}
+  @Input() product!: Product;
+  @Output() notify = new EventEmitter();
+  ngOnInit(): void {}
+}
